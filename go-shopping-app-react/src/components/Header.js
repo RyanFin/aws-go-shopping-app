794f492e8484549d1f9a4f20634f55ec0c72841e
RyanFin <ryanfinlayson125@hotmail.com>
2021-04-09 21:34:50 +0100

    Add styling

--- go-shopping-app-react/src/components/Header.js
+++ go-shopping-app-react/src/components/Header.js
@@ -1,8 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button,Container,Row,Col } from 'react-bootstrap'
+
 const Header = () => {
     return (
         <header>
-            <h1>Welcome to the Go Shopping Application</h1>
-            <h2>Take a look at our skating products</h2>
+        <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
+            <Navbar.Brand href="#home">Go Shopping App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                 <Nav.Link href="/">Home</Nav.Link>
+                {/* <Nav.Link href="/contact-us">About Us</Nav.Link> */}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+
+        <Container>
+        <Row>
+            <Col>
+            <h1 className="text-center">Welcome to the Go Shopping Application</h1>
+            <h2 className="text-center">Take a look at our skating products</h2>
+            <Button variant="outline-primary">Primary</Button>{' '}
+            </Col>
+        </Row>
+        </Container>
+
         </header>
     )
 }
